53064385fd6a880461cea6bdba5467e98ee7b413
Ryan Esmores <ryanesmores07@gmail.com>
2021-11-30 20:40:42 +0900

    Finished cocktail list project

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta incidunt
+        doloribus distinctio adipisci, delectus corporis, hic ab, quas ipsa quo
+        labore reprehenderit quia nam error. Fugit culpa iste quo quae
+        architecto obcaecati. Deserunt reiciendis magni vel voluptatibus nostrum
+        modi reprehenderit.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
